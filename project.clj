f0878c917695c9983c968fc88f4975358a6f3ec6
Michael Westbom <totallymike@gmail.com>
2015-01-30 23:01:58 -0500

    Tutorial 3 added ring/compojure

--- project.clj
+++ project.clj
@@ -4,14 +4,17 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2740"]]
+                 [org.clojure/clojurescript "0.0-2740"]
+                 [compojure "1.3.1"]]
 
   ;; CLJ AND CLJS source paths
   :source-paths ["src/clj" "src/cljs"]
 
   ;; lein-cljsbuild plugin to build a CLJS project
-  :plugins [[lein-cljsbuild "1.0.4"]]
+  :plugins [[lein-cljsbuild "1.0.4"]
+            [lein-ring "0.9.1"]]
 
+  :ring {:handler modern-cljs.core/handler}
   ;; cljsbuild options configuration
   :cljsbuild {
      :builds [{
